e1d80f53cf447297804480f99781cb584adc81b3
Gabriela Dyworová <gabriela.dyworova@gmail.com>
2022-11-08 13:02:50 +0100

    Přidejte soubory projektu.

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Ahoj, jak se jmenuješ?");
+string a = Console.ReadLine();
+Console.WriteLine("Jaký jsi?");
+string b = Console.ReadLine();
+Console.WriteLine( a + " je " + b );
+Console.ReadKey();
